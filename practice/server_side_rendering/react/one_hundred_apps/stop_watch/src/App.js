6fe81e0e53ac580f6b215a7ec10090d0736c4b63
Atsuda <aktd19910219@gmail.com>
2023-11-03 23:31:40 +0900

    stop_watch

--- practice/server_side_rendering/react/one_hundred_apps/stop_watch/src/App.js
+++ practice/server_side_rendering/react/one_hundred_apps/stop_watch/src/App.js
@@ -1,61 +1,16 @@
-import logo from "./logo.svg";
-import "./App.css";
 import React, { useState, useEffect } from "react";
 
-function TimeCoponent({ mSec }) {
-  return (
-    // mm:ss:ms の形式で表示する
-    <div>
-      {Math.floor(mSec / 60000)}:
-      {Math.floor((mSec % 60000) / 1000)
-        .toString()
-        .padStart(2, "0")}
-      :
-      {Math.floor((mSec % 1000) / 10)
-        .toString()
-        .padStart(2, "0")}
-    </div>
-  );
-}
-
-function Timer() {
-  const [mSec, setMSec] = useState(0);
-  const [isRunning, setIsRunning] = useState(false);
+export default function Example() {
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
-    let timerId;
-    if (isRunning) {
-      timerId = setInterval(() => {
-        setMSec((prev) => prev + 10);
-      }, 10);
-    }
-    return () => clearInterval(timerId);
-  }, [isRunning]);
+    document.title = `You clicked ${count} times`;
+  });
 
   return (
     <div>
-      <TimeCoponent mSec={mSec} />
-      <button onClick={() => setIsRunning(!isRunning)}>
-        {isRunning ? "Stop" : "Start"}
-      </button>
-      <button
-        onClick={() => {
-          setMSec(0);
-          setIsRunning(false);
-        }}
-      >
-        Reset
-      </button>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
     </div>
   );
 }
-
-function App() {
-  return (
-    <div className="App">
-      <Timer />
-    </div>
-  );
-}
-
-export default App;
